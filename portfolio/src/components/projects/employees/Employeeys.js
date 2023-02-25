bd832b478f2b95d644802d32f54cbda7c4029c67
Anton <toshik22sh@gmail.com>
2023-02-25 23:10:51 +0100

    fake server

--- portfolio/src/components/projects/employees/Employeeys.js
+++ portfolio/src/components/projects/employees/Employeeys.js
@@ -1,46 +1,108 @@
 import { useEffect, useState } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
 import emp from "./employees.module.css";
 
-import { add, del, inc } from "../../../store/actions";
-
 function Employees() {
-  let init = false;
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
+  const url = "http://localhost:3000/employees/";
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:3000/employees")
+    fetch(url)
       .then((response) => response.json())
-      .then((data) => {
-        if (!init) {
-          data.forEach((item) => {
-            dispatch(add(item.name, item.salary, item.hidden, item.id));
-            init = true;
-          });
-        }
-      });
+      .then((data) => setData(data));
   }, []);
 
+  async function POST() {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        name,
+        salary,
+        hidden: false,
+        id: data[data.length - 1].id + 1,
+        isPromoted: false,
+        hasBonus: false,
+      }),
+    });
+  }
   const [name, setName] = useState("");
   const [salary, setSalary] = useState(0);
-  const [promote, setPromote] = useState(false);
 
   function addEmployee() {
-    let id = state.length ? +state[state.length - 1].id + 1 : 1;
-    dispatch(add(name, salary, false, id));
+    POST();
+    setData([
+      ...data,
+      {
+        name,
+        salary,
+        hidden: false,
+        id: data[data.length - 1].id + 1,
+        isPromoted: false,
+        hasBonus: false,
+      },
+    ]);
   }
   function deleteEmployee(key) {
-    console.log(key);
-    dispatch(del(key));
+    setData(data.filter((item) => item.id !== key));
+    fetch(url + key, { method: "DELETE" });
   }
 
-  function onPromote() {
-    console.log("Inc");
-    console.log(state);
+  function onPromote(e, id) {
+    e.target.classList.toggle(emp.opacity);
+    e.stopPropagation()
+    setData(
+      data.map((item) =>
+        item.id === id ? { ...item, isPromoted: !item.isPromoted } : item
+      )
+    );
+    console.log(data.find((el) => el.id === id));
+    fetch(url + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        ...data.find((el) => el.id === id),
+        isPromoted: !data.find((el) => el.id === id).isPromoted,
+      }),
+    });
+
+  }
+  function addBonus(id) {
+    setData(
+      data.map((item) =>
+        item.id === id ? { ...item, hasBonus: !item.hasBonus } : item
+      )
+    );
+    console.log(data.find((el) => el.id === id));
+    fetch(url + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        ...data.find((el) => el.id === id),
+        hasBonus: !data.find((el) => el.id === id).hasBonus,
+      }),
+    });
+  }
+  function onPromoteFilter() {
+    setData(data.filter((item) => item.isPromoted === true));
+  }
+  function showAllEmployees() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data));
   }
 
-  return (
+  function searchFilter(e) {
+setData(data.filter(item=>(~item.name.toUpperCase().indexOf(e.target.value.toUpperCase()))))
+    console.log(e.target.value);
+  }
+  console.log(data);  return (
     <div className={emp.figure}>
       <div className={emp.container}>
         <div className={emp.header}>
@@ -48,28 +110,31 @@ function Employees() {
             <h3>Accounting of employees in the company</h3>
           </div>
           <div>
-            <h3>Total employees: {state ? state.length : 0}</h3>
+            <h3>Total employees: {data ? data.length : 0}</h3>
           </div>
           <div>
-            <h3>Will receive the Bonus: {state ? state.length : 0}</h3>
+            <h3>Will receive the Bonus: {data.filter(item => item.hasBonus).length}</h3>
           </div>
         </div>
 
         <div className={emp.filter}>
-          <input placeholder="Search"></input>
+          <input placeholder="Search" onChange={(e)=>searchFilter(e)}></input>
           <div className={emp.buttons}>
-            <button>All</button>
-            <button onClick={() => onPromote()}>Promote</button>
-            <button>More 5000</button>
+            <button onClick={() => showAllEmployees()}>All</button>
+            <button onClick={() => onPromoteFilter()}>Promote</button>
+            <button onClick={() => setData(data.filter(item => item.salary >= 5000))}>More 5000</button>
           </div>
         </div>
 
         <div className={emp.containerEmployees}>
-          {state ? (
-            state.map((item) => {
+          {data ? (
+            data.sort().map((item) => {
+
               return (
                 <div
-                  className={item.hidden ? emp.hidden : emp.employees}
+                style={item.hasBonus? {color: 'darkgoldenrod'} : {color:'black'}}
+                  className={emp.employees}
+                  onClick={() => addBonus(item.id)}
                   key={item.id}
                 >
                   <div className={emp.name}>
@@ -81,14 +146,24 @@ function Employees() {
                   <div className={emp.salary}>{item.salary}</div>
 
                   <div className={emp.buttons}>
-                    <div  onClick={()=> setPromote(!promote)}>
-                      <img className={promote? '': emp.opacity} src="/icons/coin.png" alt="logo"  />
+                    <div>
+                      <img
+                        className={item.isPromoted? '': emp.opacity}
+                        onClick={(e) => onPromote(e, item.id)}
+                        src="/icons/coin.png"
+                        alt="logo"
+                      />
                     </div>
                     <div name="delete" onClick={() => deleteEmployee(item.id)}>
                       <img src="/icons/delete.png" alt="logo" />
                     </div>
                     <div>
-                      <img src="/icons/star.png" alt="logo" />
+                      <img
+                        className={item.hasBonus? '': emp.opacity}
+                        
+                        src="/icons/star.png"
+                        alt="logo"
+                      />
                     </div>
                   </div>
                 </div>
